5be60d05585667c2781478e9496f6568728682a5
Davide Bronzini <davide_bronzini@hotmail.com>
2021-03-05 11:07:39 +0100

    editor

---
+++ src/Traits/Editor/DatatableEditorTrait.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace IlBronza\Datatables\Traits\Editor;
+
+trait DatatableEditorTrait
+{
+    static function getDatatableEditorUrl()
+    {
+        mori(static::class);
+        return 'maranza';
+    }
+}
