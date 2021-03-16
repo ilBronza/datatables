ad4e42b1bc2f485e25e8feb779340c9a53cb26b1
Davide Bronzini <davide_bronzini@hotmail.com>
2021-03-16 23:00:42 +0100

    asd

--- resources/views/datatablesFields/__tableSingleSpecCss.blade.php
+++ resources/views/datatablesFields/__tableSingleSpecCss.blade.php
@@ -3,11 +3,11 @@
 
 @foreach($table->getFields() as $field)
     @if(! empty($field->width))
-#{{ $table->getId() }} th.{{ $field->getHtmlClass() }}
+#{{ $table->getId() }} th.{{ $field->getHtmlClassForCss() }}
 {
-    width: {{ $field->width }};
+/*    width: {{ $field->width }};
     max-width: {{ $field->width }};
-}
+*/}
     @endif
 
 @endforeach
