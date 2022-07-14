66c32cfdd68ba6b039b883e4fec6623d33e59544
Davide Bronzini <davide_bronzini@hotmail.com>
2022-07-14 13:28:07 +0200

    dom settings

--- src/Traits/DatatableDomTrait.php
+++ src/Traits/DatatableDomTrait.php
@@ -44,10 +44,30 @@ trait DatatableDomTrait
         return '<"sis-buttons-header"<"sis-buttons"B><"uk-clearfix"><"sis-header uk-flex uk-flex-middle uk-child-width-1-3"<"uk-flex uk-flex-middle"ril><"tablecaption"><f>>>tip';
     }
 
+    public function setDomMode(string $mode)
+    {
+        if($mode == 'minimal')
+            return $this->setMinimalDom();
+
+        if($mode == 'none')
+            return $this->setNoneDom();
+    }
+
+    public function setDom(string $dom)
+    {
+        $this->dom = $dom;
+    }
+
     public function setMinimalDom()
     {
         //Blfritip
-        $this->dom = 'ftip';
+        $this->setDom('ftip');
+    }
+
+    public function setNoneDom()
+    {
+        //Blfritip
+        $this->setDom('tip');
     }
 
     public function getCustomDom()
