c0d6bc746b033bb5418a17c3458bd0460584e224
Davide Bronzini <davide_bronzini@hotmail.com>
2022-07-14 13:27:54 +0200

    lightbox

---
+++ src/DatatablesFields/Links/DatatableFieldLightbox.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace IlBronza\Datatables\DatatablesFields\Links;
+
+use IlBronza\Datatables\DatatablesFields\Links\DatatableFieldLink;
+
+class DatatableFieldLightbox extends DatatableFieldLink
+{
+	public function getCustomColumnDefSingleResult()
+	{
+		return "
+
+			if(item)
+			{
+				item = '<div uk-lightbox><" . $this->getHtmlTagString() . $this->getParentDataIndexString() . $this->getHtmlDataAttributesString() . " " . $this->getHtmlClassesAttributeString() . " " . $this->getTargetHtml() . " href=\"' + " . $this->getLinkUrlString() . " + '\"  data-type=\"iframe\">" . $this->getPrefix() . "" . $this->getIconHtml() . "' + " . $this->getLinkTextString() . " + '" . $this->getSuffix() . "</" . $this->getHtmlTagString() . "></div>';
+			}
+
+			else item = '';
+		";
+	}
+}
