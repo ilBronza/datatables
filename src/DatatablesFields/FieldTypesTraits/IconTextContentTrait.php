49d24ca1fef05424a3f31b87a2a427a9df224dfd
Davide Bronzini <davide_bronzini@hotmail.com>
2022-10-20 12:30:14 +0200

    font awesome icons

--- src/DatatablesFields/FieldTypesTraits/IconTextContentTrait.php
+++ src/DatatablesFields/FieldTypesTraits/IconTextContentTrait.php
@@ -13,6 +13,9 @@ trait IconTextContentTrait
 		if($this->icon ?? false)
 			return "<span uk-icon=\"{$this->icon}\"></span>";
 
+		if($this->faIcon ?? false)
+			return "<i class=\"fa-solid fa-{$this->faIcon}\"></i>";
+
 		if(! ($this->textParameter ?? false))
 			return "<span uk-icon=\"link\"></span>";
 
