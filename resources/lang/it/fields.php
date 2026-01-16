f97f3f7ba54e7be48064776ed890c1b558ce12be
DatiSistema <datisistema@gmail.com>
2026-01-16 13:19:36 +0100

    Traduzioni 1

--- resources/lang/it/fields.php
+++ resources/lang/it/fields.php
@@ -9,5 +9,45 @@ return array (
   	'mySelfDelete' => 'Elimina',
   	'mySelfUnDelete' => 'Ripristina',
 	'save' => 'Salva',
-	'mySelfClone' => 'Duplica'
+	'mySelfClone' => 'Duplica',
+
+	'client' => 'Cliente',
+	'destination' => 'Destinazione',
+
+	'mySelfSeeElement' => [
+     'noteable' => 'Annotabile',
+    ],
+
+	    'orderProductPhase' => [
+     'order' => 'Fase Ordine Prodotto',
+    ],
+
+	 'orderProductPhase' => [
+     'product' => [
+		'client' => 'Fase Ordine Prodotto',
+	 ],
+    ],
+
+	'mySelfCode' => [
+     'orderProductPhase' => [
+		'product' => 'Codice Personale',
+	 ],
+    ],
+
+	// 'fields' => [
+    //  'orderProductPhase' => [
+	// 	'order' => 'Ordine',
+	//  ],
+    // ],
+
+	//  'fields' => [
+        'orderProductPhase' => [
+            'order' => 'Ordine', // oppure "Ordinamento" a seconda del contesto
+        ],
+    // ],
+
+
+	'orderProductPhase' => [
+     'product' => 'Prodotto',
+    ],
 );
